1dc390a5e321d1aea63ad8dddc941bcbf95f92fd
uilmi <uilmi@na.edu>
2021-08-30 22:04:17 +0700

    added string vs number theory and some other thing

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-// document.getElementById("count-el").innerText = 7;
-
-
 let count = 0;
 
 let viewCounter = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-    count = count + 1;
-    viewCounter.innerText = count;
+    count += 1;
+    viewCounter.textContent = count;
 }
+
+function save() {
+    console.log(count)
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    viewCounter.textContent = 0;
+    count = 0;
+}
+
+// string vs number note
+let name = "JS Counter App"
+let greeting = "Welcome to "
+let myGreeting = greeting + name;
+// console.log(myGreeting) // string always win
+
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = "4" + "10"
+
+console.log(totalPoints)
+
+//some practice on string
+console.log(4 + 5) //9
+console.log("2" + "4") //"24"
+console.log("5" + 1) //"51"
+console.log(100 + "100") //"100100"
+
+// Welcome message render it
+welcomeEl = document.getElementById("welcome-el");
+welcomeEl.innerText = myGreeting;
+
